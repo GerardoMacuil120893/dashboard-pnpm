25941b5a42841b1e83fa40ebcc4d838441b894f3
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 <juan.orta@disrupti>
2025-05-22 17:41:59 -0600

    feature: tailwind css was added

---
+++ src/views/Login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+  return (
+    <div className="text-2xl text-green-600 w-full h-screen flex justify-center items-center bg-gray-100">
+      <div className="bg-white p-28 rounded-md border">Hello Login</div>
+    </div>
+  );
+};
